c7616fb8989c32e5819f5c1e8296d98695b058e8
Wcent <511737600@qq.com>
2020-03-29 02:06:15 +0800

    自定义数据源druid监控应用

--- src/main/java/org/cent/demo/DemoApplication.java
+++ src/main/java/org/cent/demo/DemoApplication.java
@@ -3,8 +3,11 @@ package org.cent.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+// 自定义数据源配置时，可排除默认自动配置的数据源
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan(basePackages = "org.cent.demo.mapper")
 public class DemoApplication {
 
